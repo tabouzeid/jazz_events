f8a9c56919603e1bb5b4801d07ea0619d8b8a613
Tatyana <hanyatan@gmail.com>
2020-10-06 08:36:23 -0400

    passport

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+var passport   = require('./config/passport');
+var session    = require('express-session');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,8 +13,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
-
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session());
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 // Send every other request to the React app
 // Define any API routes before this runs
