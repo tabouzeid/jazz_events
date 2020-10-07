d59c0cfd0d5c2107f12eb8808a8a1b3fe8bdf4d3
Nicole Graiff <nicole.graiff@gmail.com>
2020-10-06 23:18:49 -0400

    Starter NaviBar with Home, Login, Signup links.

--- client/src/components/NaviBar/index.js
+++ client/src/components/NaviBar/index.js
@@ -4,6 +4,8 @@ import BrickWall from "../../assets/brick_wall_copy.jpg";
 import FadeIn from 'react-fade-in';
 
 export default class NaviBar extends React.Component {
+    
+
     render() {
         const mystyle = {
             color: "white",
@@ -12,12 +14,25 @@ export default class NaviBar extends React.Component {
         };
 
         return (
-            <FadeIn>
-                <nav className="navbar" style={mystyle}>
-                    <img src={Logo} alt="bones_logo" className="my-2 ml-3" ></img>
-                    <span className="navbar-brand mx-auto">Welcome</span>
-                </nav>
-            </FadeIn >
+            <div>
+                <ul class="nav justify-content-end" id="navItems">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/login">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/signin">Signup</a>
+                        </li>
+                    </ul>
+                <FadeIn>
+                    <nav className="navbar" style={mystyle}>
+                        <img src={Logo} alt="bones_logo" className="my-2 ml-3" ></img>
+                        <span className="navbar-brand mx-auto">Welcome</span>
+                    </nav>
+                </FadeIn >
+            </div>
         );
     }
 }
