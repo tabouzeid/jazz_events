3cb83ff619873deb53444ac3374cce1509134071
Nicole Graiff <nicole.graiff@gmail.com>
2020-10-08 12:02:44 -0400

    Updated class to className for NaviBar starter. Home, Login, Signup links.

--- client/src/components/LoginPage/index.js
+++ client/src/components/LoginPage/index.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function LoginPage() {
-    return (<div></div>);
-}
+export default function LoginPage(props) {
+    console.log("The props = ", props);
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const handleClick = event => {
+        event.preventDefault();
+        }   
+
+    return (<div>
+        <form>
+            <label>Username:</label>
+            <input type="text" name="username" onSubmit={event => {setUsername(event.target.value)}}/>
+            <label>Password:</label>
+            <input type="password" name="password" onSubmit={event => {setPassword(event.target.value)}}/>
+            <button type="submit" onClick={handleClick}>Submit</button>
+        </form>
+    </div>)
+ }
