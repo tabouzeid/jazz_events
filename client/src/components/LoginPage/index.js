088c52a2d7822dc8745456dad287b7d4d76852a7
Nicole Graiff <nicole.graiff@gmail.com>
2020-10-09 12:50:30 -0400

    First pass @ Login/Signup & utils/API routes.

--- client/src/components/LoginPage/index.js
+++ client/src/components/LoginPage/index.js
@@ -1,21 +1,101 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./styles.css"
+import API from "../../utils/API";
 
-export default function LoginPage(props) {
-    console.log("The props = ", props);
+export default function LoginPage() {
+    //set states for current user's username and password
     const [username, setUsername] = useState();
     const [password, setPassword] = useState();
+    // Set states for input fields so they can be cleared onSubmit
+        //...there may be a better way to do this
+        const [userInputValue, setUserInputValue] = useState();
+        const [passInputValue, setPassInputValue] = useState();
 
-    const handleClick = event => {
+    const handleSubmit = event => {
         event.preventDefault();
+            console.log("The username = ", username, " and the password = ", password);
+        
+        // Validate form entries    
+        validateSubmit();
         }   
 
-    return (<div>
+    // Validate all form entries before querying the DB
+    const validateSubmit = () => {
+        console.log("Validating username: ", username, " password: ", password);
+
+        if (username === "" || username === undefined || username.length < 2) {
+        return alert("We don't recognize your username. Please enter a valid username");
+        } else if (password === "" || password === undefined || password.length < 8) {
+        return alert("Oops. Your password doesn't match. Please try again.")
+        } else {
+            console.log("Validation passed. Querying DB for username: ", username, " password: ", password);
+            // If all conditions pass...
+            // Query Mongo DB for the user's info using & passport(?)...
+            API.getUser({
+                username: username,
+                password: password
+            })
+            .then(() => {
+                console.log("The username = ", username, " and the password = ", password, " were queried");
+            })
+            .catch(error => {
+                console.log("There was an error: ", error);
+                alert("I'm sorry, we have encountered an error with your Login submission.");
+            })
+
+            // Clear input field values after post attempt
+            setUserInputValue("");
+            setPassInputValue("");
+        }
+    }
+    
+
+    return (
         <form>
-            <label>Username:</label>
-            <input type="text" name="username" onSubmit={event => {setUsername(event.target.value)}}/>
-            <label>Password:</label>
-            <input type="password" name="password" onSubmit={event => {setPassword(event.target.value)}}/>
-            <button type="submit" onClick={handleClick}>Submit</button>
-        </form>
-    </div>)
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <label>Username:</label>
+                        </td>
+                        <td>
+                            <input 
+                                type="text" 
+                                name="username" 
+                                placeholder="Enter username here." 
+                                value={userInputValue}
+                                onChange={event => {
+                            setUsername(event.target.value);
+                            setUserInputValue(event.target.value);
+                            }}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Password:</label>
+                        </td>
+                        <td>
+                            <input 
+                                type="password" 
+                                name="password" 
+                                placeholder="Enter password here." 
+                                value={passInputValue}
+                                onChange={event => {
+                            setPassword(event.target.value);
+                            setPassInputValue(event.target.value);
+                            }}/>     
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div>
+                <button 
+                    type="submit" 
+                    onClick={handleSubmit}
+                >
+                    Submit
+                </button>
+            </div>
+        </form> 
+    )
  }
