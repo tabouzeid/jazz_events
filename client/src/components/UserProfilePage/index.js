ae39d52304848bd6c955c214710ba22701063131
Nicole Graiff <nicole.graiff@gmail.com>
2020-10-15 20:46:09 -0400

    Temporary stylying to be redone. James' original code is commented for now.

--- client/src/components/UserProfilePage/index.js
+++ client/src/components/UserProfilePage/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./styles.css";
 // const currentPassRef = useRef();
 // const errorMsgRef = useRef();
 // const [currentPass, setCurrentPass] = useState("");
@@ -28,33 +29,121 @@ export default function UserProfilePage() {
     return (
         <div>
             <form onSubmit={updateSettings}>
-                <h1>User Settings</h1>
-
+            <h1>User Settings</h1>
+                <div className="form-group">
+                    <h3 className="heading">Change Email</h3>
+                    <b>Current email: </b> taher@taher.com <br/>
+                    <label for="newEmail">New email:</label>
+                    <input 
+                        type="email" 
+                        className="form-control" 
+                        id="newEmail" 
+                        name="email"
+                        placeholder="new@email.com"
+                        minlength="8"
+                        required />
+                </div>
+                <div className="form-group">
+                    <h3 className="heading">Change Username</h3>
+                    <b>Current username: </b> taher <br/>
+                    <label for="newUsername">New username:</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="newUsername" 
+                        name="username"
+                        placeholder="Enter new username"
+                        minlength="8"
+                        required />
+                </div>
+                <div className="form-group">
+                    <h3 className="heading">Change Password</h3>
+                    <label for="currentPassword">Current password:</label>
+                    <input 
+                        type="password" 
+                        className="form-control" 
+                        id="currentPassword" 
+                        name="currentPassword"
+                        placeholder="Enter current password"
+                        minlength="8"
+                        required />
+                    <span id="currentPasswordTest" ></span>
+                    <label for="newPassword">New password:</label>
+                    <input 
+                        type="password" 
+                        className="form-control" 
+                        id="newPassword" 
+                        name="newPassword"
+                        placeholder="Enter new password"
+                        minlength="8"
+                        required />
+                    <label for="reenterNewPassword">Reenter new password:</label>
+                    <input 
+                        type="password" 
+                        className="form-control" 
+                        id="reenterNewPassword" 
+                        name="reenterNewPassword"
+                        placeholder="Reenter new password"
+                        minlength="8"
+                        required />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+            //Leaving James' original code in case my reformatting above broke anything...
+            /* <form onSubmit={updateSettings}>
+            <div className="form-group">
                 <h3>Change email</h3>
-                    Current email: <b>taher@taher.com</b><br />
+                <h3>Current email:</h3> <b>taher@taher.com</b>
+                <div className="form-group">
                 <label for="oldPass">New email:</label>
-                <input type="text" id="newEmail" name="email"
-                    minlength="8" required /><br />
-
+                <input 
+                    type="text" 
+                    id="newEmail" 
+                    name="email"
+                    minlength="8" 
+                    required />
+                </div>
+                <div className="form-group">
                 <h3>Change username</h3>
                     Current username: <b>taher</b><br />
                 <label for="oldPass">New username:</label>
-                <input type="text" id="newUsername" name="username"
-                    minlength="8" required /><br />
-
+                <input 
+                    type="text" 
+                    id="newUsername" 
+                    name="username"
+                    minlength="8" 
+                    required /><br />
+                </div>
                 <h3>Change password</h3>
+                <div className="form-group">
                 <label for="currentPassword">Current password:</label>
-                <input type="password" id="currentPassword" name="currentPassword"
-                    minlength="8" required /><br />
+                <input 
+                    type="password" 
+                    id="currentPassword" 
+                    name="currentPassword"
+                    minlength="8" 
+                    required /><br />
                 <span id="currentPasswordTest" ></span>
+                </div>
+                <div className="form-group">
                 <label for="newPassword">New password:</label>
-                <input type="password" id="newPassword" name="newPassword"
-                    minlength="8" required /><br />
+                <input 
+                    type="password" 
+                    id="newPassword" 
+                    name="newPassword"
+                    minlength="8" 
+                    required /><br />
                 <label for="reenterNewPassword">Re-enter new password:</label>
-                <input type="password" id="reenterNewPassword" name="reenterNewPassword"
-                    minlength="8" required />
-            </form>
-        </div>
+                <input 
+                    type="password" 
+                    id="reenterNewPassword" 
+                    name="reenterNewPassword"
+                    minlength="8" 
+                    required />
+                </div>
+            </div>
+            </form> */
     );
 
 }
