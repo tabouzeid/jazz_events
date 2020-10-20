e7e941f438676dcab5fc26037a70e5ee11618f8b
Taher Abouzeid <tabouzeid@gmail.com>
2020-10-20 18:13:49 -0400

    cleanup

--- client/src/components/Files-upload-component/index.js
+++ client/src/components/Files-upload-component/index.js
@@ -28,23 +28,16 @@ export default class FilesUploadComponent extends Component {
             <div className="container mt-5">
                 <h1 className="text-center">User Settings</h1>
                 <form className="my-0 pt-3 pb-0 ">
-
-
                     <div className="d-flex justify-content-center mb-4 mt-2">
                         <img src={this.props.user.profileImg}
                             className="mx-auto d-block" style={style} alt="avatar" />
                     </div>
-
-
                     <div className="d-flex flex-column mx-auto w-50">
                         <label for="files" className="btn text-white col-6 mx-auto" style={{ backgroundColor: "#0060a4" }}>Select Image</label>
                         <input id="files" style={{ visibility: "hidden" }} type="file" onChange={this.onFileChange} className="px-0 mx-0" />
                     </div>
-
-
                 </form>
             </div>
-
         )
     }
 }
